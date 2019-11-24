cf6947ebbbda7309aa248b845ae3fe9f5d8c8182
Shokufe <shokufeusefi96@gmail.com>
2019-11-24 16:37:47 +0330

    add lifecycles methods

--- app/src/main/java/com/usefi/lifecyclelogsapp/MainActivity.java
+++ app/src/main/java/com/usefi/lifecyclelogsapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.usefi.lifecyclelogsapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,50 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.d("Log1", "onCreate method.");
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("Log1", "onStart method.");
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("Log1", "onResume method.");
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("Log1", "onPause method.");
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("Log1", "onStop method.");
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("Log1", "onDestroy method.");
+
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d("Log1", "onRestart method.");
+
     }
 }
